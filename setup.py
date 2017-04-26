d8ff7c2055480d38149aab10f36a3ed961da3728
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-04-26 21:02:39 +0200

    #N/A: Add astunparse to deps

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1'
 
-install_requires = ['typed-ast', 'typed-astunparse', 'autopep8', 'colorama']
+install_requires = ['typed-ast', 'astunparse', 'typed-astunparse',
+                    'autopep8', 'colorama']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
-                  ':python_version<"3.5"': ['typing'],}
+                  ':python_version<"3.5"': ['typing']}
 
 setup(name='py-backwards',
       version=VERSION,
