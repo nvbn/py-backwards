07c2fd5d298ded5eb863fddb6ece68e5f54758b4
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-05-22 13:50:01 +0200

    #N/A: Fix packages in `setup.py`

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-VERSION = '0.6'
+VERSION = '0.6.1'
 
 install_requires = ['typed-ast', 'autopep8', 'colorama', 'py-backwards-astunparse']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
@@ -14,7 +14,7 @@ setup(name='py-backwards',
       author_email='nvbn.rm@gmail.com',
       url='https://github.com/nvbn/py-backwards',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'example', 'tests', 'tests.*']),
+      packages=find_packages(exclude=['ez_setup', 'example*', 'tests*']),
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
