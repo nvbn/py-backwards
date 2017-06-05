4d500ce37fef41c72922b5784f3c889ead91c6eb
Thomas Grainger <tom@procensus.com>
2017-06-05 22:05:52 +0100

    add asyncio import

--- py_backwards/transformers/async_await.py
+++ py_backwards/transformers/async_await.py
@@ -1,14 +1,29 @@
 from typed_ast import ast3 as ast
 from .base import BaseNodeTransformer
 
+
+ASYNCIO_MODULE = '__py_backwards_asyncio__'
+
 def asyncio_decorator():
     return ast.Attribute(
         value=ast.Name(
-            id='asyncio',
+            id=ASYNCIO_MODULE,
             ctx=ast.Load(),
         ),
         attr='coroutine',
     )
+
+
+def splice(fn, vars):
+    for i, v in enumerate(vars):
+        nv = fn(v)
+        if nv is None:
+            yield v
+        else:
+            yield nv
+            for v in vars[i:]:
+                yield v
+            break
 
 
 class AsyncAwaitTransformer(BaseNodeTransformer):
@@ -17,13 +32,22 @@ class AsyncAwaitTransformer(BaseNodeTransformer):
             await foo()
     To
 
-        import asyncio
+        import asyncio as __py_backwards_asyncio__
 
-        @asyncio.coroutine
+        @__py_backwards_asyncio__.coroutine
         def ham():
             yield from foo()
     """
     target = (3, 4)
+
+    def visit_Module(self, n: ast.Module) -> ast.Module:
+        self._tree_changed = True
+        def dosplice(v):
+            # insert just after the first node that's not a future import.
+            if not (isinstance(v, ast.ImportFrom) and v.module == '__future__'):
+                return ast.Import(names=[ast.alias('asyncio', ASYNCIO_MODULE)])
+
+        return self.generic_visit(ast.Module(body=list(splice(dosplice, n.body))))
 
     def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> ast.FunctionDef:
         self._tree_changed = True
--- tests/transformers/test_async_await.py
+++ tests/transformers/test_async_await.py
@@ -6,16 +6,18 @@ from py_backwards.transformers.async_await import AsyncAwaitTransformer
     ('''
 async def fn():
     await range(10)
-    ''', '''
-@asyncio.coroutine
+    ''', '''import asyncio as __py_backwards_asyncio__
+
+@__py_backwards_asyncio__.coroutine
 def fn():
     yield from range(10)
 '''),
     ('''
 async def fn():
     a = await range(10)
-    ''', '''
-@asyncio.coroutine
+    ''', '''import asyncio as __py_backwards_asyncio__
+
+@__py_backwards_asyncio__.coroutine
 def fn():
     a = yield from range(10)
 '''),
